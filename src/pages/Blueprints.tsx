7dc51bb244991e2dd5f5987d3b65d44d0f253c53
BT <tekwanib7@gmail.com>
2026-09-25 14:15:33 +0100

    fix: remove unused blueprint import

--- src/pages/Blueprints.tsx
+++ src/pages/Blueprints.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   ArrowRight,
   CheckCircle2,
